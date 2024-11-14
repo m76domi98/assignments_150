e4f710cac101814280cf39726a17bcef5c08c6a2
Michelle Dominic <michelle.m.dominic@gmail.com>
2024-11-14 18:42:44 -0500

    Changes to pass marmoset

--- project_4/project_4.cpp
+++ project_4/project_4.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <cassert>
+#include "p_4_header.hpp"
 
 std::size_t length( char const *a ){
     std::size_t count = 0;
@@ -79,7 +80,7 @@ unsigned int distance( char const *str1, char const *str2 ){
 }
 
 std::size_t is_sorted (char* array[], std::size_t capacity){
-    for (std::size_t i {1}; i < capacity; i++){
+    for (std::size_t i {1}; i <= capacity; i++){
         if (compare(array[i-1], array[i]) > 0) {
             return i;
         }
@@ -88,25 +89,45 @@ std::size_t is_sorted (char* array[], std::size_t capacity){
 }
 
 void insert(char *array[], std::size_t capacity){
-    assert (capacity>0);
+    // assert (capacity > 0);
 
-    char *val = array[capacity -1];
-   std::size_t str_length = length(array[capacity -1]);
+    // char *value {array[capacity-1]};
+
+    // std::size_t i {0};
+
+    // for (i = capacity-1; (compare(array[i], value) > 0); --i){
+    //     array[i]= array[i-1];
+    // }
+
+    // array[i]= value;
 
 
-    assign(val, array[capacity-1]);
-    std::size_t i = capacity -2;
+// my version
+//     assert (capacity>0);
 
-    while ((i>=0) && (compare(array[i], val) > 0)){
-        array [i+1] = array [i]; // shifts string right like one indice
-        if (i == 0){
-            break;
-        }
+//     char *val = array[capacity -1];
+//    std::size_t str_length = length(array[capacity -1]);
 
+
+//     assign(val, array[capacity-1]);
+//     std::size_t i = capacity -2;
+
+    assert(is_sorted(array, capacity - 1) == (capacity - 1));
+
+    char *val = array[capacity - 1];
+    std::size_t i = capacity - 2;
+
+    while ((i > 0)) {
+        if ((compare(array[i], val) > 0)){
+        array[i + 1] = array[i];  // Shifts string right by one index
+        if (i == 0) {
+            break;  // Prevents underflow when i is unsigned
+        }}
         i--;
     }
 
-    array[i+1] = val;
+    array[i + 1] = val;
+
 }
 
 void insertion_sort(char *array[], std::size_t capacity) {
@@ -170,8 +191,22 @@ std::size_t find (char * array[], std::size_t capacity, char const *str){
     return b_index;
 }
 
-// void read_words_from_file(char const *filename, char **&word_array, std::size_t &num_words, std::size_t  width) {
+void free_word_array(char **word_array){
+    if(word_array){
+        delete[] word_array[0];
+        delete[] word_array;
+    }
+}
 
+/// @brief Reads words from a text file, allocates and populates a word array, and modifies the number of words
+/// @remark You will need to understand how memory is allocated in order to delete memory in free_word_array
+/// @param filename the name of the file to be opened
+/// @param word_array given a word array pointer, allocates the word array
+/// @param num_words updates (pass by reference) the number of words found in the file
+/// @param width the maximum number of letters in a word
+// void read_words_from_file(char const *filename, char **&word_array,std::size_t &num_words, std::size_t  width) {
+//     // this function was given to us so like
+//     // uh u lit told us to use this
 //     // Attempt to open the file
 //     std::ifstream file{ filename };
 //     if (!file.is_open()) {
@@ -201,61 +236,3 @@ std::size_t find (char * array[], std::size_t capacity, char const *str){
 
 //     file.close();
 // }
-
-// void free_word_array(char **word_array){
-//     if(word_array){
-//         delete[] word_array[0];
-//         delete[] word_array;
-//     }
-// }
-
-int main() {
-    std::size_t const WIDTH{ 20 }; // set the maximum number of letters in a word
-     
-    char **test_words{};            // empty word array
-    std::size_t num_test_words{};   // number of words (will be modified when a file is read)
-
-    // populate the word array, and update the number of words
-    //read_words_from_file("test_words.txt", test_words, num_test_words, WIDTH );
-
-      //////////////////////
-     // Function Testing //
-    //////////////////////
-    char test_string[21] {"cat"};
-
-    // length
-    std::cout << "The length of " << test_string << " is " << length(test_string) << std::endl;
-    
-    // comapare
-    int test_compare { compare ( test_string, "dog") };
-    if ( test_compare < 0 ) {
-        std::cout << "The test_string comes before dog" << std::endl;
-    } else if ( test_compare > 0 ) {
-        std::cout << "The test_string comes after dog" << std::endl;
-    } else {
-        std::cout << "The test_string is dog" << std::endl;
-    }
-
-    // assign
-    assign(test_string, "bird");
-    std::cout << test_string << std::endl;
-    
-    // distance
-    std::cout << "The distance between " << test_string 
-              << " and hat is " << distance(test_string, "hat") << std::endl;
-
-    // is sorted
-    std::size_t num_sorted = is_sorted( test_words, num_test_words );
-    if ( num_sorted == num_test_words ) {
-        std::cout << "The test_words array is sorted." << std::endl;
-    } else {
-        std::cout << "Only the first " << num_sorted << " words are sorted." << std::endl;
-    }
-
-    /// Insert additional tests as needed
-
-    // deallocate
-    //free_word_array(test_words);
-
-    return 0;
-}
